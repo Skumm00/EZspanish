0675d7e6011bea8b5c1de903c2a82f7229b93914
Skand Bajpai <99634361+Skumm00@users.noreply.github.com>
2024-07-24 20:22:41 +0000

    removed old nav and made new one and started hero. Now going to make responsive and have a better design.

---
+++ public/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('hidden');
+});

80dac190c23f99556e1323e67df81d2357825bf0
Skand Bajpai <99634361+Skumm00@users.noreply.github.com>
2024-07-27 21:40:28 +0000

    Fixed a bit of the UI and added more logic in the js

--- pages/quiz.js
+++ pages/quiz.js
@@ -56,3 +56,32 @@ const questions = [
   },
   
 ]
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextBtn = document.getElementById("next");
+const previousBtn = document.getElementById("previous");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+  currentQuestionIndex = 0;
+  score = 0;
+  nextBtn.innerHTML = "next";
+  previousBtn.innerHTML = "previous";
+  showQuestion();
+}
+
+function showQuestion() {
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex +1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+  });
+}

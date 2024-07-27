d54e04e6dd7397742b63448c605725c7e05a39b6
Skand Bajpai <99634361+Skumm00@users.noreply.github.com>
2024-07-27 20:25:29 +0000

    Added the js code to a new folder and also changed the code. Finished adding questions

---
+++ pages/quiz.js
@@ -0,0 +1,58 @@
+//question list
+const questions = [
+  {
+    question:"What Type of Framework is Django?",
+    answers: [
+      {text:"Web Framework", correct: false},
+      {text:"Backend Framework", correct: true},
+      {text:"Frontend Framework", correct: false},
+      {text:"Full Stack Framework", correct: false},
+    ]
+  },
+  {
+    question:"What Language Type is python?",
+      answers: [
+        {text:"Backend Language", correct: true},
+        {text:"OOP Language", correct: false},
+        {text:"Frontend Language", correct: false},
+        {text:"Full Stack Language", correct: false},
+      ]
+  },
+  {
+    question:"What does OOP stand for?",
+      answers: [
+        {text:"Olive Orange Programing", correct: false},
+        {text:"Olympic Ozempic Program", correct: false},
+        {text:"Objected Orientated Programming", correct: true},
+        {text:"Full Stack Framework", correct: false},
+      ]
+  },
+  {
+    question:"How do check for bugs?",
+      answers: [
+        {text:"Check the console", correct: true},
+        {text:"Check the Terminal", correct: false},
+        {text:"Check the code", correct: false},
+        {text:"Check GIT", correct: false},
+      ]
+  },
+  {
+    question:"What is the command to push a commit to the main/local repo?",
+      answers: [
+        {text:"git push ", correct: false},
+        {text:"git pull", correct: false},
+        {text:"git force", correct: false},
+        {text:"git push orgin main", correct: true},
+      ]
+  },
+  {
+    question:"What is git?",
+      answers: [
+        {text:"A system to commit and record changes ", correct: true},
+        {text:"A amazing food to eat", correct: false},
+        {text:"A sysrem to avoid hackers and copyright", correct: false},
+        {text:"A website", correct: false},
+      ]
+  },
+  
+]
